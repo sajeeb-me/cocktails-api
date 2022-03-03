aee789dca42cb3c4ee6030515154f5552eb24852
sdshuvo98 <hellosdshuvo@gmail.com>
2022-03-04 01:12:34 +0600

    model added

--- js/app.js
+++ js/app.js
@@ -16,7 +16,7 @@ const getItems = items => {
     items.forEach(item => {
         const div = document.createElement("div");
         div.innerHTML = `
-        <div class="col" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+        <div onclick="getDetails(${item.idDrink})" class="col" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
             <div class="card h-100">
                 <img src="${item.strDrinkThumb}" class="item-image card-img-top" alt="...">
                 <div class="card-body">
@@ -29,4 +29,37 @@ const getItems = items => {
         `;
         showCaseItems.appendChild(div);
     })
+}
+
+const getDetails = id => {
+    const url = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`
+    fetch(url)
+        .then(res => res.json())
+        .then(data => showInModal(data.drinks[0]))
+}
+
+const showInModal = details => {
+    const modalInner = document.getElementById("modal-inner");
+    modalInner.textContent = '';
+    const div = document.createElement("div");
+    div.innerHTML = `
+    <div class="back-background modal-content">
+    <div class="modal-header">
+    <h5 class="modal-title" id="staticBackdropLabel">${details.strDrink}</h5>
+    <button type="button" class="btn-close btn-close-white" data-bs-dismiss="modal" aria-label="Close"></button>
+    </div>
+    <div class="modal-body">
+    <div class=""><img src="${details.strDrinkThumb}" class="item-image card-img-top" alt="...">
+    <p><small>Category : ${details.strCategory}</small></p>
+    <p>${details.strInstructions}</p>
+    </div>
+    <div></div>
+    </div>
+    <div class="modal-footer">
+    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+    <a href="${details.strImageSource}" target="_blank"><button type="button" class="btn btn-primary">Order now</button></a>
+    </div>
+    </div>
+    `;
+    modalInner.appendChild(div)
 }
